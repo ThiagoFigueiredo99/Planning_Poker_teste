90e3bad9e020e9b57049c0ecb2cfc79484ea692b
nick nick <nicholascaitong@gmail.com>
2023-04-24 21:49:51 -0300

    add js to load modals

---
+++ JS/classes.js
@@ -0,0 +1,16 @@
+class Loader {
+    constructor(selector) {
+        this.elemento = document.querySelector(selector);
+    }
+    render(caminhoDoArquivo) {
+        fetch(caminhoDoArquivo)
+            .then(response => response.text())
+            .then(html => {
+                this.elemento.innerHTML = html;
+            });
+    }
+
+    limpar() {
+        this.elemento.innerHTML = '';
+    }
+}
