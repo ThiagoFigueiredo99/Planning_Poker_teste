fb5636674f3cd2c6567276cb10f7906e680aa978
Thiago Figueiredo <th.leonardo99@gmail.com>
2023-04-24 22:06:38 -0300

    2° release / javascript
    add javascript nos modals
    melhorando o design das telas

---
+++ JS/index.js
@@ -0,0 +1,45 @@
+(() => {
+    const l = new Loader(".loader")
+
+    function loadNewVote() {
+        l.render("/modal-votacao.html")
+    }
+
+    function closeVote(){
+        l.limpar();
+    }
+
+    function confirmButton(){
+        const title = document.querySelector('#title').value
+        const description = document.querySelector('#description').value
+        
+        let votes = localStorage.getItem('votes')
+        
+        if (votes === null) {
+          votes = '[]';
+          localStorage.setItem('votes', votes);
+        }
+        
+        let jvotes = JSON.parse(votes);
+        
+        if (!Array.isArray(jvotes)) {
+          jvotes = [];
+        }
+        
+        const novoObjeto = { title, description };
+        jvotes.push(novoObjeto);
+        
+        localStorage.setItem('votes', JSON.stringify(jvotes));
+
+        l.limpar();
+    }
+
+
+    
+    document.addEventListener('click', (e) => {
+        if(e.target.classList.contains("newVote")){ loadNewVote() }
+        if(e.target.classList.contains("cancel")){ closeVote() }
+        if(e.target.classList.contains("confirm")){ confirmButton() }
+    })
+
+})();
